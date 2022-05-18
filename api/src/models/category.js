ccf067135b011badd3e105638f4e6ff4d3e81838
Julian Rondina <55722212+rondinajulian@users.noreply.github.com>
2022-05-18 11:37:44 -0300

    Update category.js

--- api/src/models/category.js
+++ api/src/models/category.js
@@ -1,40 +1,14 @@
-const { DataTypes } = require('sequelize');
-// Exportamos una funcion que define el modelo
-// Luego le injectamos la conexion a sequelize.
-module.exports = (sequelize) => {
-  // defino el modelo
+const { Sequelize,DataTypes } = require('sequelize');
 
-  sequelize.define("videogame", {
-    id: {
-      type: DataTypes.UUID,
-      defaulValue: DataTypes.UUIDV4,
-      primaryKey: true,
-    },
+module.exports = (sequelize) => {
+
+  sequelize.define('category', {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
-    },
-    description: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    image: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    platforms: {
-      type: DataTypes.TEXT,
-      AllowNull: false,
-    },
-    released: {
-      type: DataTypes.DATE,
-      allowNull: true,
-    },
-    rating: {
-      type: DataTypes.INTEGER,
-    },
-  }, {timestamps: true,
-    createdAt: 'lanzamiento',
-    updatedAt: false
-});
+      primaryKey: true,
+    }
+  },{
+    timestamps:true
+  });
 };
