80c742025d8845e50ff90d4d681dc4f70df7c8f8
Julian Rondina <55722212+rondinajulian@users.noreply.github.com>
2022-05-18 11:41:23 -0300

    Update category.js

--- api/src/models/category.js
+++ api/src/models/category.js
@@ -3,11 +3,22 @@ const { Sequelize,DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
 
   sequelize.define('category', {
+     id: {
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
-      primaryKey: true,
-    }
+    },
+    description: {
+      type: DataTypes.TEXT,
+    },
+     image: {
+      type: DataTypes.TEXT,
+    },
   },{
     timestamps:true
   });
