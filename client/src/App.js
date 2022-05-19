4f939c573c8f641bf97f91dcfec0b00be9504853
Midasisking <testasanty@gmail.com>
2022-05-19 14:39:29 -0300

    Feat(App - Rendering Landing and Home)

--- client/src/App.js
+++ client/src/App.js
@@ -5,30 +5,22 @@ import { Routes, Route, useLocation } from "react-router-dom";
 // Hooks
 
 // Components
+import Landing from './components/Landing.jsx'
+import Home from './components/Home.jsx'
 
 function App() {
 
-  console.log('Just trying something, if you see this you can delete it')
-
-  
   const {pathname} = useLocation()
   return (
     <div className="App">
-      {pathname === '/'?null:<Nav/>}
+      {/* {pathname === '/'?null:<Nav/>} */}
       <Routes>
         <Route path='/' element={<Landing/>}/>
-        <Route path="/home" element={<Home 
-        onSearch={onSearch} handleDetails={handleDetails} 
-        allPosts={meal} loading={loading} 
-        />}/>
-        <Route path="/create" element={<Create />}/>
-        <Route path='/details' element={<Details id={id}/>}/>
-        <Route path='*' element={<NotFound/>}/>
+        <Route path='/home' element={<Home/>}/>
+        {/* <Route pathelement={<NotFound/>}/> */}
       </Routes>
     </div>
   );
 }
 
 export default App;
-
-//what happens if i add this line
