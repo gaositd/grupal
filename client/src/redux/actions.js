c1ba3457194b7c4a81ac0e46b0ec58aea233f4f6
Midasisking <testasanty@gmail.com>
2022-05-19 23:40:31 -0300

    Feat(Actions and reducer function get product by id - missing api endpoint in actions)

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,11 +1,20 @@
 import axios from "axios"
 
 export const GET_SOMETHING = "GET_SOMETHING"
+export const GET_PRODUCT_ID = "GET_PRODUCT_ID"
 
 export const getRecipes = (query) => {
     return function(dispatch){
         return axios.get(`http://localhost:3001/something`, {params: {query: query}})
             .then(resp => dispatch({type: GET_SOMETHING, payload: resp.data}))
             .catch(error => alert('Error in getSomething: ',error))
+    }
+}
+
+export const getProductById = (id) => {
+    return function(dispatch){
+        return axios.get(`http://localhost:3001/recipe/${id}`)
+            .then(resp => dispatch({type: GET_PRODUCT_ID, payload: resp.data}))
+            .catch(error => alert(error))
     }
 }
--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -1,15 +1,19 @@
 import {
-    GET_SOMETHING
+  GET_SOMETHING,
+  GET_PRODUCT_ID
 } from "./actions"
 
 const initialState={
-    
+  products: [],
+  productDet: {}
 }
 
 export function rootReducer(state = initialState, {type, payload}){
-    switch (type) {
-        case GET_SOMETHING:
-            return {...state, something: payload}
-        default: return state;
-    }
+  switch (type) {
+    case GET_SOMETHING:
+      return {...state, something: payload}
+    case GET_PRODUCT_ID:
+      return {...state, recipe: payload}
+    default: return state;
+  }
 }
