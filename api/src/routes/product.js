8b10913f875cceae6cbda482a4bc6b621729895e
rondinajulian <55722212+rondinajulian@users.noreply.github.com>
2022-05-23 11:38:13 -0300

    filter products by category

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -1,20 +1,123 @@
 const { Router } = require("express");
-const axios = require("axios").default;
-const { v4: uuidv4 } = require("uuid");
-const { product } = require("../db.js");
-var validate = require("uuid-validate");
-
 const router = Router();
+const { v4: uuidv4 } = require("uuid");
+//const { getProduct, getProducts, createProduct, updateProduct, deleteProduct } = require("../controllers/product");
+const { Product } = require("../db.js");
 
-router.post("/", async (req, res, next) => {
-  
+router.get("/", async (req, res, next) => {
+    const getProduct = await Product.findAll();
+    res.send(getProduct);
 });
+
+
+
+router.get("/:idProduct", async (req, res, next) => {
+    console.log(req.query);
+    console.log(req.params);
+    const { idProduct } = req.params;
+
+    try {
+        if (idProduct) {
+            console.log(idProduct)
+            const product = await Product.findOne({
+                where: {
+                    id: idProduct
+                }
+            });
+            
+            if (product) {
+                res.json(product);
+            } else {
+                res.status(404).json({
+                    message: "Product not found"
+                });
+            }
+        } else {
+            
+            res.status(400).json({
+                message: "Bad request"
+            });
+            
+            
+        } 
+
+
+        //const product = await Product.findByPk(req.params.idProduct);
+        
+    } catch (error) {
+        next(error);
+    }
+
+});
+
+
+
+
 
 //________________________________________________________________//
 
-router.get("/:idProduct", async (req, res, next) => {
-res.send(req.params.idProduct);  
+router.post("/", async (req, res, next) => {
+    let {name, description, image, ranking, createBy, price, category, stock} = req.body;
+        
+    //console.log(req.body);
+    try {
+        
+        const productCreated = await Product.findOrCreate({
+            where: {
+                id: uuidv4(),
+                name,
+                description,
+                image,
+                ranking,
+                createBy,
+                price,
+                category,
+                stock
+                
+                
+            }
+        })
+        res.send(productCreated);
+        //res.send(productCreated[0].category);
+        await productCreated.setCategories(productCreated.category);
+        
+        // console.log(productCreated);
+        // await productCreated[0].setCategories(category)
+        
+    } catch (error) {
+        
+    }
+    // res.send('Created succesfully, saludos desde el BACK!!')
+
+    
 });
+
+router.get('/category/:category', async (req,res)=>{
+    const {category} = req.params;
+  
+    const products = await Product.findAll();
+  
+    
+  
+    try {
+        const productByCategory =
+        products.filter((p)=>{
+            const productFilter = p;
+            categoryFilter = productFilter.category
+            if(categoryFilter.includes(category)) return productFilter;
+        }) 
+    
+    
+    res.json(productByCategory)
+        
+    } catch (error) {
+        res.send(error)
+        
+    }
+  
+  
+  
+  })
 
 
 module.exports = router;
