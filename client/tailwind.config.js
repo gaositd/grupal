408cc95f7e3668bc3ef865f9ce7b1c02b86b12d7
Felipe De la Paz <felipedelapaz@gmail.com>
2022-05-25 01:52:00 -0400

    landing page

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+      fontFamily: {
+        sans: ["Montserrat", "sans-serif"]
+      },
+    },
+    plugins: [],
+  }
 }
