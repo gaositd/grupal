60fe28e64e8f32067c8ce5987c7fd1d6cc9d2859
sedwarb <sedwarb@gmail.com>
2022-05-27 22:31:22 -0500

    changed model name

---
+++ api/src/models/user.js
@@ -0,0 +1,49 @@
+const { Sequelize, DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  
+  sequelize.define('user', {
+    id: {
+      type: DataTypes.STRING,
+      //type: DataTypes.UUID,
+      //defaultValue: Sequelize.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    nickName: {    //2
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    fullName: {       //3
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    mail: {       //8
+      type: DataTypes.TEXT,
+    },
+    phone: {      //6
+      type: DataTypes.STRING,
+      AllowNull: false,
+    },
+    address: {      //5
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    birthdate: {  //4
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    country: {    //7
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    verify: {  //11
+      type: DataTypes.BOOLEAN,
+
+    },
+    
+  }, {timestamps: true,
+    createdAt: true ,
+    updatedAt: false
+});
+};
