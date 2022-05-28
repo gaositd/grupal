b979624c98f4ac340fbe659820a7a95e8dc06455
sedwarb <sedwarb@gmail.com>
2022-05-27 22:33:57 -0500

    add new route post review, changed route get producto By ID

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -3,7 +3,8 @@ const router = Router();
 const { v4: uuidv4, validate } = require("uuid");
 const { Op } = require("sequelize");
 //const { getProduct, getProducts, createProduct, updateProduct, deleteProduct } = require("../controllers/product");
-const { Product, Category } = require("../db.js");
+const { Product, Category, User, Review } = require("../db.js");
+const {createReview}=require('../controllers/controllers')
 
 router.get("/", async (req, res, next) => {
     const getProduct = await Product.findAll();
@@ -21,15 +22,13 @@ router.get("/:idProduct", async (req, res, next) => {
     const { name } = req.query;
     console.log('ID ',idProduct);
     console.log('NOMBRE ',name);
-    
     if (validate(idProduct)) {
-        console.log('CONSULTA POR PARAMS CON ID :',idProduct);
         try {
             if (idProduct) {
                 const product = await Product.findByPk(idProduct,{
-                    include: Category,
+                    include: [{model:Category},{model:Review}]
                 });
-                const { id, name, description, image, ranking, createBy, price, stock, categories } = product;
+                const { id, name, description, image, ranking, createBy, price, stock, categories,reviews } = product;
                 const response = {
                     id,
                     name,
@@ -40,7 +39,14 @@ router.get("/:idProduct", async (req, res, next) => {
                     price,
                     stock,
                     categories: categories.map(category => category.name),
-                    }
+                    reviews: reviews.map(review => {
+                        return {
+                            description:review.description,
+                            ranking:review.ranking,
+                            userId:review.userId
+                        }
+                    })
+                }
                 res.json(response);
             } else {
                 res.send("idProduct is required");
@@ -48,11 +54,8 @@ router.get("/:idProduct", async (req, res, next) => {
         } catch (error) {
             next(error);
         }
-
-
     }else{
         if(name) {
-            console.log('CONSULTA POR QUERY CON NOMBRE :',name);
             const searchDbNames = await Product.findAll({
                 where: {
                   name: { [Op.iLike]: `%${name}%` },
@@ -72,13 +75,10 @@ router.get("/:idProduct", async (req, res, next) => {
                     categories: product.categories.map(category => category.name),
                 }
             });
-
             res.status(200).json(finalProduct);
         }else{
-
             const getProduct = await Product.findAll({
                 include: Category,
-        
             });
             let finalProduct = getProduct.map(product => {
                 return {
@@ -95,7 +95,6 @@ router.get("/:idProduct", async (req, res, next) => {
             });
             res.status(200).json(finalProduct);
         }
-        
     }
 });
 
@@ -103,30 +102,19 @@ router.get("/:idProduct", async (req, res, next) => {
 
 router.get('/category/:category', async (req,res)=>{
     const {category} = req.params;
-  
     const products = await Product.findAll();
-  
-    
-  
     try {
         const productByCategory =
         products.filter((p)=>{
             const productFilter = p;
             categoryFilter = productFilter.category
             if(categoryFilter.includes(category)) return productFilter;
-        }) 
-    
-    
+        })
     res.json(productByCategory)
-        
     } catch (error) {
         res.send(error)
-        
     }
-  
-  
-  
-  })
+})
 
 
 
@@ -135,27 +123,27 @@ router.get('/category/:category', async (req,res)=>{
 //para hacer post de un producto
 // hacer la llamada a localhost:3001/product
 // enviar el body de la siguiente forma:
-// {
-//     "name":"Nuevo Producto 11",
-//     "description":"Descripcion producto 11",
-//     "image":"imagen producto 1",
-//     "ranking":"10",
-//     "createBy":"Mariano",
-//     "price":"202",
-//     "categories":["2","3"],
-//     "stock":"10"
-//   }
-  
+/*
+{
+     "name":"Nuevo Producto 11",
+     "description":"Descripcion producto 11",
+     "image":"imagen producto 1",
+     "ranking":"10",
+    "createBy":"Mariano",
+     "price":"202",
+     "categories":["2","3"],
+     "stock":"10"
+}
+*/
+//para hacer post de reviews se debe colocar localhost:3001/product/review
+router.post("/review", createReview)
 
 router.post("/", async (req, res, next) => {
     let {name, description, image, ranking, createBy, price, categories, stock} = req.body;
-        
-    console.log('REQ.Body Productos :',req.body);
     try {
-        
         const productCreated = await Product.findOrCreate({
             where: {
-                id: uuidv4(),
+                id: req.body.id,
                 name,
                 description,
                 image,
@@ -163,25 +151,13 @@ router.post("/", async (req, res, next) => {
                 createBy,
                 price,                
                 stock
-                
-                
             }
         })
-        
-        // console.log('DATOS DEL PRODUCTO a CREAR',productCreated);
         await productCreated.addCategories(categories);
         res.json(productCreated);
-
-        
     } catch (error) {
-        res.send(error)
-        
+        res.send(`Error: ${error}`)
     }
-    // res.send('Created succesfully, saludos desde el BACK!!')
-
-    
-
-
 });
 
 
