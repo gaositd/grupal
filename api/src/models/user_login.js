0bc881d04c49e638b0bd6193b539f807263ea75b
carmarfuc <mariano.fuchilieri@gmail.com>
2022-05-30 22:59:07 -0300

    ajuste de rutas user

--- api/src/models/user_login.js
+++ api/src/models/user_login.js
@@ -16,6 +16,41 @@ module.exports = (sequelize) => {
     usertype:{
       type:DataTypes.STRING,
       allowNull: false,
-    }
-  });
+    },
+    nickName: {    //2
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    fullName: {       //3
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    mail: {       //8
+      type: DataTypes.TEXT,
+    },
+    phone: {      //6
+      type: DataTypes.STRING,
+      AllowNull: false,
+    },
+    address: {      //5
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    birthdate: {  //4
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    country: {    //7
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    verify: {  //11
+      type: DataTypes.BOOLEAN,
+  },
+  },
+  {timestamps: true,
+  createdAt: true ,
+  updatedAt: false}
+  
+  );
 }
